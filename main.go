b2c5655e54db3f32c3af944bfd34c71df629431a
notriv <notriv@ogame.ninja>
2024-10-25 15:45:06 -0700

    update patch

--- main.go
+++ main.go
@@ -33,27 +33,33 @@ func processManifest(by []byte) []byte {
 }
 
 func processBackgroundJs(by []byte) []byte {
-	by = replN(by, `var a = /s(\d+)-(\w+)\.ogame\.gameforge\.com/.exec(t.url),`, `const universeNum = /browser\/html\/s(\d+)-(\w+)/.exec(t.url)[1];
+	by = replN(by, `var a=/s(\d+)-(\w+)\.ogame\.gameforge\.com/.exec(t.url),`, `const universeNum = /browser\/html\/s(\d+)-(\w+)/.exec(t.url)[1];
 const lang = /browser\/html\/s(\d+)-(\w+)/.exec(t.url)[2];`, 1)
-	by = replN(by, `a[1] + "-" + a[2];`, `universeNum + "-" + lang;`, 1)
+	by = replN(by, `a[1]+"-"+a[2];`, `universeNum + "-" + lang;`, 1)
+	by = replN(by, `"https://"+o.ogameURL.replace("s{UID}","*")+"/*"`, `"*://*/*"`, 1)
+	by = replN(by, `initialize(t){`, `initialize(t, u){const myUrl = new URL(u);const universeNum = /browser\/html\/s(\d+)-(\w+)/.exec(u)[1];
+const lang = /browser\/html\/s(\d+)-(\w+)/.exec(u)[2];`, 1)
+	by = replN(by, `universe.initialize(s)`, `universe.initialize(s, t.url)`, 1)
+	by = replN(by, `this.origin[t]="https://"+o.ogameURL.replace("{UID}",t)`, `this.origin[t]=myUrl.origin+"/api/s" + universeNum + "/" + lang + "/"`, 1)
+	by = replN(by, `+"/api/"+`, `+`, 2)
 	return by
 }
 
 func processUniverseviewJs(by []byte) []byte {
-	by = replN(by, `const e = /s(\d+)-(\w+)\.ogame\.gameforge\.com/.exec(a.location.host);`, `const universeNum = /browser\/html\/s(\d+)-(\w+)/.exec(a.location.href)[1];
+	by = replN(by, `const e=/s(\d+)-(\w+)\.ogame\.gameforge\.com/.exec(a.location.host);`, `const universeNum = /browser\/html\/s(\d+)-(\w+)/.exec(a.location.href)[1];
 const lang = /browser\/html\/s(\d+)-(\w+)/.exec(a.location.href)[2];`, 1)
-	by = replN(by, `u.OGAMELEGACY = /^\d/.exec(d.version)[0] < 7`, `u.OGAMELEGACY = false`, 1)
-	by = replN(by, `d.universeNumber = e[1], d.community = e[2]`, `d.universeNumber = universeNum, d.community = lang`, 1)
+	by = replN(by, `u.OGAMELEGACY=/^\d/.exec(d.version)[0]<7`, `u.OGAMELEGACY = false`, 1)
+	by = replN(by, `d.universeNumber=e[1],d.community=e[2]`, `d.universeNumber = universeNum, d.community = lang`, 1)
 	by = replN(by, `/game/index.php`, ``, 4)
-	by = replN(by, `a.location = a.location.origin + `, `a.location =`, 2)
-	by = replN(by, `href = "index.php?`, `href = "?`, 2)
-	by = replN(by, `checkXML: function(e, t, i) {`, `{old} const universeNum = /browser\/html\/s(\d+)-(\w+)/.exec(a.location.href)[1];
+	by = replN(by, `a.location=a.location.origin+`, `a.location =`, 2)
+	by = replN(by, `href="index.php?`, `href = "?`, 2)
+	by = replN(by, `checkXML:function(e,t,i){`, `{old} const universeNum = /browser\/html\/s(\d+)-(\w+)/.exec(a.location.href)[1];
+const lang=/browser\/html\/s(\d+)-(\w+)/.exec(a.location.href)[2];`, 1)
+	by = replN(by, `fetchXML:function(e,t,i,n){`, `{old} const universeNum = /browser\/html\/s(\d+)-(\w+)/.exec(a.location.href)[1];
 const lang = /browser\/html\/s(\d+)-(\w+)/.exec(a.location.href)[2];`, 1)
-	by = replN(by, `fetchXML: function(e, t, i, n) {`, `{old} const universeNum = /browser\/html\/s(\d+)-(\w+)/.exec(a.location.href)[1];
-const lang = /browser\/html\/s(\d+)-(\w+)/.exec(a.location.href)[2];`, 1)
-	by = replN(by, `url: (i || a.location.origin) + "/api/" + t,`, `
+	by = replN(by, `url:(i||a.location.origin)+"/api/"+t,`, `
 url: (i || a.location.origin) + "/api/s" + universeNum + "/" + lang  + "/" + t,`, 1)
-	by = replN(by, `url: (n || a.location.origin) + "/api/" + t,`, `
+	by = replN(by, `url:(n||a.location.origin)+"/api/"+t,`, `
 url: (n || a.location.origin) + "/api/s" + universeNum + "/" + lang  + "/" + t,`, 1)
 	return by
 }
